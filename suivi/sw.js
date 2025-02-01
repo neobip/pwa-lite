4c11cf3fb5ccad2de68f77af5cbe46adf28c77fd
neobip <neobip@users.noreply.github.com>
2025-02-01 16:55:26 +0100

    Update sw.js

--- suivi/sw.js
+++ suivi/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open("suivi-cache").then((cache) => {
-            return cache.addAll(["index.html"]);
+            return cache.addAll(["index.htm"]);
         })
     );
 });
